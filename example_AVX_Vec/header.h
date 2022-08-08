290c271fc1e93c1f12d33868b7590d79577542a0
Shidur Rahman <shidureh@yahoo.co.uk>
2022-08-08 22:12:19 +0100

    2X2MVM implemented

--- example_AVX_Vec/header.h
+++ example_AVX_Vec/header.h
@@ -26,4 +26,8 @@ void single_MVM(suNf_vector *chi, const suNf *up, const suNf_vector *psi);
 void double_MVM(suNf_vector *chi, suNf_vector *chi2, const suNf *up, const suNf_vector *psi, const suNf_vector *psi2);
 void single_MVM_inverse(suNf_vector *chi, const suNf *um, const suNf_vector *psi);
 void double_MVM_inverse(suNf_vector *chi, suNf_vector *chi2, const suNf *um, const suNf_vector *psi, const suNf_vector *psi2);
+void single_MVM_2x2(suNf_vector *chi, const suNf *up, const suNf_vector *psi);
+void double_MVM_2x2(suNf_vector *chi, suNf_vector *chi2, const suNf *up, const suNf_vector *psi, const suNf_vector *psi2);
+void single_MVM_inverse_2x2(suNf_vector *chi, const suNf *um, const suNf_vector *psi);
+void double_MVM_inverse_2x2(suNf_vector *chi, suNf_vector *chi2, const suNf *um, const suNf_vector *psi, const suNf_vector *psi2);
 int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y);
