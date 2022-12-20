756c47fb3efcb04ca972c5e35cf4e24e0916c9d2
MD Rahman <mrahman@foseres.localdomain>
2022-12-20 12:22:20 +0000

    "Change for slrum Job Array Creation"

--- example_AVX_Vec/run_speedtest.sh
+++ example_AVX_Vec/run_speedtest.sh
@@ -1,28 +1,42 @@
-#!/bin/bash  # tells the shell what  shell to run the script under, here bash shell.  
-MAXCORE=16  # MAXCORE = 16 : Number of cores per socket as accordance with foseres system. You are putting here the number of cores of 1 socket of the processor that I am using and that should generate all the runs I can think.  
-NSOCKETS=2  
+#!/bin/bash
+MAXCORE=16
+NSOCKETS=2
 NODES=$1
 localt=(4 6 8 10)
 locall=6
 MAXT=120
+counter=0
 
 EXEC="speed_test_diracoperator"
 outfile="out_0_n${NODES}"
 reportfile="report_nodes${NODES}.csv"
-jobmpi="job_n${NODES}.mpi"
 
 ((MAXPROC = NSOCKETS * MAXCORE * NODES))
 ((MAXPROCPERNODE = NSOCKETS * MAXCORE))
 
 parse_out_script="./parse_out.sh"
 
+jobmpi="job_${NODES}.mpi"
 rm -rf $jobmpi
 
-echo "export  OMP_PROC_BIND=close" > $jobmpi
-echo "export  OMP_PLACES=cores" >> $jobmpi
-echo "scontrol show hostname \$SLURM_JOB_NODELIST | perl -ne 'chomb; print \"\$_\"x1'> myhostall_nodes${NODES}">> $jobmpi
+#echo "export  OMP_PROC_BIND=close" > $jobmpi
+#echo "export  OMP_PLACES=cores" >> $jobmpi
+#echo "scontrol show hostname \$SLURM_JOB_NODELIST | perl -ne 'chomb; print \"\$_\"x1'> myhostall_nodes${NODES}">> $jobmpi
+                cat <<EOF >$jobmpi
+#!/bin/bash
 
+#SBATCH -J openmpi_test               # Job name
+#SBATCH -o job.%j.out         # Name of stdout output file (%j expands to jobId)
+#SBATCH -N $NODES                  # Total number of nodes requested
+#SBATCH --time 48:00:00           # Run time (hh:mm:ss) - 1.5 hours
+#SBATCH --exclusive
+#SBATCH --qos=scavenger
+# Launch MPI-based executable
 
+module load GCC/11.2.0   
+module load  OpenMPI/4.1.1
+EOF
+		
 cat <<EOF >$reportfile
 #Number of Threads
 #Gauge group
@@ -68,8 +82,8 @@ for paral in $(seq 0 3); do
         break
     fi
 
-    ((maxnpt = MAXPROC / (2 ** paral) ))
-    ((procpernode = MAXPROCPERNODE / (2 ** paral) ))
+    ((maxnpt = MAXPROC / (2 ** paral)))
+    ((procpernode = MAXPROCPERNODE / (2 ** paral)))
 
     if ((procpernode == 0)); then
         break
@@ -81,11 +95,11 @@ for paral in $(seq 0 3); do
     NPTLIST=$(seq 1 $maxnpt)
 
     for npt in ${NPTLIST[*]}; do
-        (( ( ( ( npt * NPX * NPY * NPZ ) / 2 ) * 2 ) != npt*NPX*NPY*NPZ )) && continue
-        (( ( ( ( npt * NPX * NPY * NPZ ) / NODES ) * NODES ) != npt*NPX*NPY*NPZ )) && continue
-        (( npt * NPX * NPY * NPZ  < NODES )) && continue        
+        (((((npt * NPX * NPY * NPZ) / 2) * 2) != npt * NPX * NPY * NPZ)) && continue
+        (((((npt * NPX * NPY * NPZ) / NODES) * NODES) != npt * NPX * NPY * NPZ)) && continue
+        ((npt * NPX * NPY * NPZ < NODES)) && continue
         for lct in ${localt[*]}; do
-            ((glbt = npt * lct ))
+            ((glbt = npt * lct))
             ((glbt > MAXT)) && continue
             cat <<EOF >loc_speed_${NODES}_${npt}_${lct}_${paral}.in
 // Global variables 
@@ -108,15 +122,28 @@ log:default = -1
 log:inverter = -1
 log:forcestat = 0
 EOF
-            omplist=(`seq 2 2 $maxompproc`)
+            omplist=($(seq 2 2 $maxompproc))
             omplist=(1 ${omplist[@]})
             for ompproc in ${omplist[@]}; do
-                (( ompproc * npt * NPX * NPY * NPZ >  MAXPROC )) && continue
-                echo export OMP_NUM_THREADS=$ompproc >>$jobmpi
-                echo rm -f ${outfile}_${npt}_${lct}_${ompproc} >>$jobmpi
-                echo mpirun  -hostfile myhostall_nodes${NODES} -n $(( npt * NPX * NPY * NPZ )) --map-by node ./$EXEC -i loc_speed_${NODES}_${npt}_${lct}_${paral}.in -o ${outfile}_${NODES}_${npt}_${lct}_${paral}_${ompproc} >>$jobmpi
-                echo -e "$parse_out_script ${outfile}_${NODES}_${npt}_${lct}_${paral}_${ompproc} >>$reportfile\n" >>$jobmpi
+                ((ompproc * npt * NPX * NPY * NPZ > MAXPROC)) && continue
+		(( counter+=1 ))
+		if (( counter == 1)) ; then
+		    ifelse="if"
+		else
+                    ifelse="elif"
+		fi
+                cat <<EOF >>$jobmpi
+$ifelse (( SLURM_ARRAY_TASK_ID == $counter )) ; then
+export OMP_NUM_THREADS=$ompproc
+rm -f ${outfile}_${NODES}_${npt}_${lct}_${paral}_${ompproc} 
+mpirun -n $((npt * NPX * NPY * NPZ)) --map-by node ./$EXEC -i loc_speed_${NODES}_${npt}_${lct}_${paral}.in -o ${outfile}_${NODES}_${npt}_${lct}_${paral}_${ompproc}
+$parse_out_script ${outfile}_${NODES}_${npt}_${lct}_${paral}_${ompproc} >>$reportfile
+EOF
+                #sbatch $jobmpi
             done
         done
     done
 done
+
+echo "fi" >> $jobmpi      
+echo "Jobarray should contain $counter elements"
